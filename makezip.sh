9913485076c0b88623a9ebf6ea524287aef692e3
klingbeek <k.lingbeek@geonovum.nl>
2019-11-13 13:05:35 +0100

    Root opgeschoond

--- makezip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -f BesluitInitieleRegeling-bsl2-Gemeentestad.xml
-cat BesluitInitieleRegeling-bsl2-Gemeentestad.xml.TEMPLATE | sed s/XXVERSIEXX/$RANDOM/g  > BesluitInitieleRegeling-bsl2-Gemeentestad.xml
-rm -f BesluitGemeentestad.zip
-zip BesluitGemeentestad.zip opdracht.xml manifest-ow.xml manifest.xml BesluitInitieleRegeling-bsl2-Gemeentestad.xml owActiviteiten-Gemeentestad-1-template.xml owBeperkingengebied-Gemeentestad-1-template.xml owFuncties-Gemeentestad-1-template.xml owLocaties-Gemeentestad-1-template.xml owLocaties-Gemeentestad-2-template.xml owNormen-Gemeentestad-1-template.xml owRegeltekst-Gemeentestad-1-template.xml
